f3bf23ad941d8e444baa9412c9b8ad742352f9c6
SergeHenrich <mindjosserge2@gmail.com>
2026-04-24 12:53:40 +0100

    feat: add .htaccess files for Angular routing and Laravel API security

--- AgoraFront/Frontend/src/app/services/api.ts
+++ AgoraFront/Frontend/src/app/services/api.ts
@@ -1,7 +1,7 @@
 //src/services/api.ts
 export const API_CONFIG = {
-  baseUrl: 'http://localhost:8000/api',
+  baseUrl: 'https://groupe4.cdwfs.net/groupe4/api',
   // Tu peux aussi ajouter l'URL de base pour les images/logos ici
-  storageUrl: 'http://localhost:8000/storage'
+  storageUrl: 'https://groupe4.cdwfs.net/groupe4/api/storage'
 };
 // src/app/services/api.ts
