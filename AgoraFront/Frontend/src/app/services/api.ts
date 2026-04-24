2a32be6750a3d69f5f4075c6afa6e0a311fc1a73
SergeHenrich <mindjosserge2@gmail.com>
2026-04-24 14:21:07 +0100

    Update api.ts

--- AgoraFront/Frontend/src/app/services/api.ts
+++ AgoraFront/Frontend/src/app/services/api.ts
@@ -1,7 +1,7 @@
 //src/services/api.ts
 export const API_CONFIG = {
-  baseUrl: 'https://groupe4.cdwfs.net/groupe4/api',
+  baseUrl: 'https://groupe4.cdwfs.net/groupe4/api/',
   // Tu peux aussi ajouter l'URL de base pour les images/logos ici
-  storageUrl: 'https://groupe4.cdwfs.net/groupe4/api/storage'
+  storageUrl: 'https://groupe4.cdwfs.net/groupe4/api/storage/'
 };
 // src/app/services/api.ts
